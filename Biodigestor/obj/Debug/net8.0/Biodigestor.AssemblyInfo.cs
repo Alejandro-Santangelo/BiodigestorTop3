8d0b14bc7f7e446c9b5c6c695056a812204caa0b
Alejandro-Santangelo <artesaniaslarustica@gmail.com>
2024-09-27 05:56:56 -0300

    Agregado de RegistroController y sus get

--- Biodigestor/obj/Debug/net8.0/Biodigestor.AssemblyInfo.cs
+++ Biodigestor/obj/Debug/net8.0/Biodigestor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biodigestor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f53dea042db0941fe5a54a6652046ae04710a9a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dd0da7c98d6109e650206c0e8ea491b27182641")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biodigestor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biodigestor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
